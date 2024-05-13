7b62985323c8f84851aa55afde34cc0fbe127e11
vj-sonawane <vijayshreeacademy20@gmail.com>
2024-05-14 01:29:43 +0530

    Initial Commit

---
+++ src/test/java/com/vscode/springcloudconfigserverdemo/ConfigServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vscode.springcloudconfigserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
